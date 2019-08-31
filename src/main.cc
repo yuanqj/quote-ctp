4f3e7685cf30c4e0976aa9ff068b83ccdb59b010
yuanqj <yuanqj8191@gmail.com>
2019-08-31 11:37:45 +0800

    Mask input password

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,14 @@
 #include <csignal>
 #include <iostream>
+#include <termios.h>
+#include <unistd.h>
 #include <uuid/uuid.h>
 #include <cmdline/cmdline.h>
 #include "quote_client.hh"
 
 std::string *gen_uuid();
 cmdline::parser* config_cli(int argc, char** argv);
+std::string get_password();
 std::vector<std::string> *parse_instruments(const std::string *str, char sep);
 void term_sig_handler(int signum);
 
@@ -15,24 +18,25 @@ QuoteClient *client;
 int main(int argc, char** argv) {
     cmdline::parser *params = config_cli(argc, argv);
     std::string *uuid = gen_uuid();
-    std::cout << std::endl << "Quote-CTP Starts: " << *uuid << std::endl;
-    signal(SIGINT, term_sig_handler);
-    signal(SIGTERM, term_sig_handler);
+    std::cout << "Quote-CTP Starts: " << *uuid << std::endl;
 
     auto instruments = parse_instruments(&params->get<std::string>("instruments"), ';');
+    auto pwd = get_password();
     client = new QuoteClient(
             uuid,
             &params->get<std::string>("broker"),
             &params->get<std::string>("investor"),
-            &params->get<std::string>("password"),
+            &pwd,
             &params->get<std::string>("front-addr"),
             instruments,
             &params->get<std::string>("path-conn"),
             &params->get<std::string>("path-data")
     );
+    signal(SIGINT, term_sig_handler);
+    signal(SIGTERM, term_sig_handler);
     client->run();
 
-    std::cout << std::endl << "Quote-CTP Ends: " << *uuid << std::endl << std::endl;
+    std::cout << "Quote-CTP Ends: " << *uuid << std::endl;
     delete(client);
     delete(params);
     delete(instruments);
@@ -44,7 +48,6 @@ cmdline::parser *config_cli(int argc, char** argv) {
     auto *params = new cmdline::parser();
     params->add<std::string>("broker", 0, "Broker ID", true, "");
     params->add<std::string>("investor", 0, "Investor ID", true, "");
-    params->add<std::string>("password", 0, "Investor password", true, "");
     params->add<std::string>("front-addr", 0, "Front server address", true, "");
     params->add<std::string>("instruments", 0, "Instruments to subscribe which separated with ';'", true, "");
     params->add<std::string>("path-conn", 0, "File path for storing connection flow", true, "");
@@ -54,8 +57,22 @@ cmdline::parser *config_cli(int argc, char** argv) {
 }
 
 void term_sig_handler(int signum) {
-    std::cout << std::endl << "Termination signal received ["<< signum <<"]." << std::endl;
+    std::cout << "Termination signal received ["<< signum <<"]." << std::endl;
     client->stop();
+}
+
+std::string get_password() {
+    std::cout << "Input password for CTP account: ";
+    termios t0 = {0};
+    tcgetattr(STDIN_FILENO, &t0);
+    termios t1 = t0;
+    t1.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &t1);
+
+    std::string pwd;
+    getline(std::cin, pwd);
+    std::cout << std::endl;
+    return pwd;
 }
 
 std::vector<std::string> *parse_instruments(const std::string *str, char sep) {
